409d25e0349b1caf4786c2bd0f8f29d395c6d7fe
progre <djyayutto@gmail.com>
2017-06-03 11:02:01 +0900

    feat: add say

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function main() {
   cron('0 0 23 * * *', () => {
     console.log('say This server will shutdown at 24 o\'clock.')
   });
+  cron('0 30 23 * * *', () => {
+    console.log('say This server will shutdown at 24 o\'clock.')
+  });
+  cron('0 50 23 * * *', () => {
+    console.log('say This server will shutdown at 24 o\'clock.')
+  });
   cron('0 59 23 * * *', () => {
     console.log('say This server will shutdown in 1 minutes.')
   });
